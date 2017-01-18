64bb9a280b510f08665f7582b100420d13c8455b
bharding2 <b.harding@gmail.com>
2017-01-17 17:27:38 -0800

    placeholder route model

--- app/routes/issues.js
+++ app/routes/issues.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      'Number of open issues that were opened in the last 24 hours: ' + '0',
+      'Number of open issues that were opened in the last 7 days: ' + '0',
+      'Number of open issues that were opened more than 7 days ago: ' + '0'
+    ];
+  }
 });
