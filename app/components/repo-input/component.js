c7157820cf821280993920d84edc6e5d8b0d3acc
bharding2 <b.harding@gmail.com>
2017-01-23 16:29:12 -0800

    adding some date logic

--- app/components/repo-input/component.js
+++ app/components/repo-input/component.js
@@ -8,10 +8,23 @@ export default Ember.Component.extend({
       var userOrg = splitData[1];
       var repoName = splitData[2];
 
-      var repoUrlString = 'https://api.github.com/repos/' + userOrg + '/' + repoName + '/issues';
+      var repoUrlString = 'https://api.github.com/repos/' + userOrg + '/' + repoName + '/issues?state=all';
+
+      var issuesSince24Hrs = 0;
+      var issuesSince7Days = 0;
+      var issuesBefore7Days = 0;
+      var todayBase = new Date();
+
+      console.log(todayBase);
 
       Ember.$.getJSON(repoUrlString, function(data) {
         console.log(data);
+        data.forEach((ele) => {
+          if (Date.parse(ele.created_at) >= todayBase - 86400000) issuesSince24Hrs++;
+          if (Date.parse(ele.created_at) >= todayBase - 604800000) issuesSince7Days++;
+          if (Date.parse(ele.created_at) <= todayBase - 604800000) issuesBefore7Days++;
+        });
+        console.log(issuesSince24Hrs, issuesSince7Days, issuesBefore7Days);
       });
     }
   }
